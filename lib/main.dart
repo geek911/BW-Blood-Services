66594b956e89d8c3b0093022f6d7be51986b2daa
Moses <moseschawawa@gmail.com>
2021-04-21 15:48:37 +0200

    Added Firebase to the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
-void main() {
-  runApp(App());
+Future<void> main() async {
+  try{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    runApp(App());
+  }catch(e){
+    print(e);
+  }
+
 }
